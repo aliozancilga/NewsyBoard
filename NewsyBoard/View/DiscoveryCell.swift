54aeafb89f9c07bfa31c41bdb457940d95a9a5e4
Blingdenstone <ali.ozan.cilga@gmail.com>
2018-09-14 07:56:35 +0300

    added Discoverycollectionviewcontroller, DiscoveryCell, gitignore

---
+++ NewsyBoard/View/DiscoveryCell.swift
@@ -0,0 +1,13 @@
+//
+//  DiscoveryCell.swift
+//  NewsyBoard
+//
+//  Created by Macbook on 14.09.2018.
+//  Copyright © 2018 Ali Ozan. All rights reserved.
+//
+
+import UIKit
+
+class DiscoveryCell: UICollectionViewCell {
+    
+}
